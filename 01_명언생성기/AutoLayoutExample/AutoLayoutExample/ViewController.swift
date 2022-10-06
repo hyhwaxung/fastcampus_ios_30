7522334c3fdd5dc9087b1fba1afcfb3a0cd5be38
hyhwaxung <hyhwaxung@gmail.com>
2022-10-06 20:32:19 +0900

    2022.10.06 IBOutlet & IBAction

--- 01_명언생성기/AutoLayoutExample/AutoLayoutExample/ViewController.swift
+++ 01_명언생성기/AutoLayoutExample/AutoLayoutExample/ViewController.swift
@@ -14,6 +14,11 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBOutlet weak var colorView: UIView!
+    @IBAction func tapChangeColorButton(_ sender: UIButton) {
+        self.colorView.backgroundColor = UIColor.blue
+        print("색상 변경 버튼이 클릭되었음")
+    }
+    
 }
 
