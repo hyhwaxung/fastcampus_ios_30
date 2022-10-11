9c4547840bd0d4846152f0e1ed713afb51624f82
hyhwaxung <hyhwaxung@gmail.com>
2022-10-11 13:44:45 +0900

    2022.10.11 일기 앱 기능 구현 (1)

---
+++ 05_일기/Diary/Diary/Diary.swift
@@ -0,0 +1,15 @@
+//
+//  Diary.swift
+//  Diary
+//
+//  Created by 정현화 on 2022/10/11.
+//
+
+import Foundation
+
+struct Diary {
+    var title: String
+    var contents: String
+    var date: Date
+    var isStar: Bool
+}
--- 05_일기/Diary/Diary/ViewController.swift
+++ 05_일기/Diary/Diary/ViewController.swift
@@ -9,11 +9,96 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    private var diarylist = [Diary]() {
+        didSet {
+            self.saveDiaryList()
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.configureCollectionView()
+        self.loadDiaryList()
+    }
+    
+    private func configureCollectionView() {
+        self.collectionView.collectionViewLayout = UICollectionViewFlowLayout()
+        self.collectionView.contentInset = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
+        self.collectionView.delegate = self
+        self.collectionView.dataSource = self
     }
 
-
+    // segueway 방식으로 화면 이동하기 때문에 필요
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let writeDiaryViewcontroller = segue.destination as? WriteDiaryViewController {
+            writeDiaryViewcontroller.delegate = self
+        }
+    }
+    
+    private func dateToString(date: Date) -> String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yy년 MM월 dd일(EEEEE)"
+        formatter.locale = Locale(identifier: "ko_KR")
+        return formatter.string(from: date)
+    }
+    
+    private func saveDiaryList() {
+        let date = self.diarylist.map {
+            [
+                "title": $0.title,
+                "contents": $0.contents,
+                "date": $0.date,
+                "isStar": $0.isStar
+            ]
+        }
+        let userDefaults = UserDefaults.standard
+        userDefaults.set(date, forKey: "diaryList")
+    }
+    
+    private func loadDiaryList() {
+        let userDefaults = UserDefaults.standard
+        guard let data = userDefaults.object(forKey: "diaryList") as? [[String: Any]] else { return }
+        self.diarylist = data.compactMap {
+            guard let title = $0["title"] as? String else { return nil }
+            guard let contents = $0["contents"] as? String else { return nil}
+            guard let isStar = $0["isStar"] as? Bool else { return nil }
+            guard let date = $0["date"] as? Date else { return nil }
+            return Diary(title: title, contents: contents, date: date, isStar: isStar)
+        }
+        self.diarylist = self.diarylist.sorted(by: {
+            $0.date.compare($1.date) == .orderedDescending
+        })
+    }
 }
 
+extension ViewController: WriteDiaryViewDelegate {
+    func didSelectRegister(diary: Diary) {
+        self.diarylist.append(diary)
+        self.collectionView.reloadData()
+        self.diarylist = self.diarylist.sorted(by: {
+            $0.date.compare($1.date) == .orderedDescending
+        })
+    }
+}
+
+extension ViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.diarylist.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "DiaryCell", for: indexPath) as? DiaryCell else { return UICollectionViewCell() }
+        let diary = self.diarylist[indexPath.row]
+        cell.titleLabel.text = diary.title
+        cell.dateLabel.text = self.dateToString(date: diary.date)
+        return cell
+    }
+}
+
+extension ViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: (UIScreen.main.bounds.width / 2) - 20 , height: 200)
+    }
+}
