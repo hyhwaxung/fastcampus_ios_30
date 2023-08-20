6bdcb681bf6a21e77b71fb6cdeeac2476c67a131
hyhwaxung <hyhwaxung@gmail.com>
2023-08-20 11:34:03 +0900

    2023.08.20 뽀모도로 타이머 앱 기능 구현 (2) - 시작/일시정지/취소에 따른 DispatchSourceTimer 구현

--- 06_뽀모도로 타이머/pomodoro/pomodoro/ViewController.swift
+++ 06_뽀모도로 타이머/pomodoro/pomodoro/ViewController.swift
@@ -26,6 +26,8 @@ class ViewController: UIViewController {
     // 60으로 초기화 하는 이유는 DatePicker의 default 값이 1min 이기 때문에
     var duration = 60
     var timerStatus: TimerStatus = .end
+    var timer: DispatchSourceTimer?
+    var currentSeconds = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -42,14 +44,39 @@ class ViewController: UIViewController {
         self.toggelButton.setTitle("일시정지", for: .selected) // 선택된 상태면 "일시정지
     }
     
+    func startTimer() {
+        if self.timer == nil {
+            self.timer = DispatchSource.makeTimerSource(flags: [], queue: .main)
+            self.timer?.schedule(deadline: .now(), repeating: 1) // 3초 후에 실행시키고 싶으면 .now() + 3 이런식으로 가능
+            self.timer?.setEventHandler(handler: { [weak self] in
+                self?.currentSeconds -= 1
+                debugPrint(self?.currentSeconds)
+                
+                if self?.currentSeconds ?? 0 <= 0 {
+                    self?.stopTimer()
+                }
+            })
+            self.timer?.resume()
+        }
+    }
+    
+    func stopTimer() {
+        if self.timerStatus == .pause {
+            self.timer?.resume()
+        }
+        self.timerStatus = .end
+        self.cancelButton.isEnabled = false
+        self.setTimerInfoViewVisible(isHidden: true)
+        self.datePicker.isHidden = false
+        self.toggelButton.isSelected = false
+        self.timer?.cancel()
+        self.timer = nil
+    }
+    
     @IBAction func tapCancelButton(_ sender: Any) {
         switch self.timerStatus {
         case .start, .pause:
-            self.timerStatus = .end
-            self.cancelButton.isEnabled = false
-            self.setTimerInfoViewVisible(isHidden: true)
-            self.datePicker.isHidden = false
-            self.toggelButton.isSelected = false
+            self.stopTimer()
             
         default:
             break
@@ -61,22 +88,23 @@ class ViewController: UIViewController {
         
         switch self.timerStatus {
         case .end:
+            self.currentSeconds = self.duration
             self.timerStatus = .start
             self.setTimerInfoViewVisible(isHidden: false)
             self.datePicker.isHidden = true
             self.toggelButton.isSelected = true
             self.cancelButton.isEnabled = true
+            self.startTimer()
         
         case .start:
             self.timerStatus = .pause
             self.toggelButton.isSelected = false
+            self.timer?.suspend()
             
         case .pause:
             self.timerStatus = .start
             self.toggelButton.isSelected = true
-            
-        default:
-            break
+            self.timer?.resume()
         }
     }
     
